42e6892fd1bd1f2a4387c2e55beb80a3b4e56dbc
monik <Alex.uma2007@yandex.ru>
2025-09-02 23:34:33 +0500

    added name field for user

--- internal/domain/models/models.go
+++ internal/domain/models/models.go
@@ -3,5 +3,6 @@ package models
 type User struct {
 	TelegramID string `json:"tgID" gorm:"primaryKey"`
 	Role       string `json:"role" gorm:"default:student"`
+	Name       string `json:"name"`
 	Language   string `json:"language" gorm:"default:ru"`
 }
